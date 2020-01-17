34695c0259e37a3aefa03e3aca2cf4a94e4fd366
Gabriel Martins <gsmartins96@hotmail.com>
2020-01-17 09:40:25 -0300

    :heavy_plus_sign: add nodemailer and config file to this

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1d84694c5bf549',
+    pass: '8d575f9a61d82c',
+  },
+  default: {
+    from: 'GymPoint <noreply@gympoint.com>',
+  },
+};
